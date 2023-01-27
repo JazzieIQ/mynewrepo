b2d12286e5af66680cc94f400d3cad5fb840de77
Matthew Gutierrez <110790606+JazzieIQ@users.noreply.github.com>
2023-01-26 16:55:05 -0800

    hello world Java update

--- helloworld.js
+++ helloworld.js
@@ -9,6 +9,21 @@
 // References:
 // https://www.w3schools.com/java/java_user_input.asp
 
+import java.util.Scanner; // import the Scanner class 
+
+class Main {
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);
+    String userName;
+    
+    // Enter username and press Enter
+    System.out.println("Enter username"); 
+    userName = myObj.nextLine();   
+       
+    System.out.println("Username is: " + userName);        
+  }
+}
+
 import java.util.Scanner;
 public class HelloWorld01 {
     public static void main(String[] args) {
